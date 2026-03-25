ad85a08c2b03764d343c61f21ade79a98c7d1555
Discord Bot Developer <developer@example.com>
2026-03-25 18:54:51 +0200

    Fix make_favicon.js: copy PNG favicon to public (reliable)

--- images/make_favicon.js
+++ images/make_favicon.js
@@ -1,17 +1,9 @@
-const sharp = require('sharp');
 const fs = require('fs');
 const path = require('path');
 
-async function makeFavicon() {
-  try {
-
-      .ico()
-      .toFile('images/icon/favicon.ico');
-    fs.copyFileSync('images/icon/favicon.ico', path.join('web_subs', 'public', 'favicon.ico'));
-    console.log('Favicon.ico generated and copied to web_subs/public/favicon.ico');
-  } catch (err) {
-    console.error('Error:', err);
-  }
+try {
+  fs.copyFileSync('images/icon/icon.png', path.join('web_subs', 'public', 'favicon.png'));
+  console.log('Favicon PNG copied to web_subs/public/favicon.png');
+} catch (err) {
+  console.error('Error:', err);
 }
-
-makeFavicon();
